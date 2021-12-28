cd6d33a347b7888a6e0a011fd0a58092994b4c67
mancongchua <96785459+mancongchua@users.noreply.github.com>
2021-12-28 22:42:31 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,20 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
+    titleWeb: "Chào cục dàng của em",
+    introTitle: 'Em là bé iu của anh nè',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
-}
+    btnIntro: 'iu lúm lúm luôn đó',
+    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên
+    Phải chăng em đã say ngay từ lúc thấy nụ cười ấy',
+    desc: 'Anh iu bé nhìu hok ',
+    btnYes: 'Iu bé rất rất nhiều',
+    btnNo: 'có chứ bé iu',
+    question: 'Trên thế giới hơn 7 tỉ người mà sao anh lại yêu em <3',
+    btnReply: 'bởi vì em cute dễ thương xinh đẹp thông minh tốt bungh',
+    reply: 'Yêu thì yêu mà không yêu thì yêu ',
+    mess: ' em cũng iu anh lúm á',
+    messDesc: 'mình sẽ gặp nhau nhaaa',
+    btnAccept: 'hun bé thật nhìuuuu',
+    messLink: 'https://www.facebook.com/trantrieu.man308/
